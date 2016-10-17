761181f289898f2253c72891f69d7473239230d6
Jorge <jordimart83@gmail.com>
2016-10-18 00:06:08 +0200

    Aun habia un fallo en ladirección actualizo y compruebo

--- modules/products/model/BLL/product_bll.class.singleton.php
+++ modules/products/model/BLL/product_bll.class.singleton.php
@@ -2,10 +2,10 @@
 
 $path = $_SERVER['DOCUMENT_ROOT'] ;
 define('SITE_ROOT', $path);
-define('MODEL_PATH', SITE_ROOT . 'model/');
+define('MODEL_PATH', SITE_ROOT . '/model/');
 
-require (MODEL_PATH . "/Db.class.singleton.php");
-require(SITE_ROOT . "/modules/products/model/DAO/product_dao.class.singleton.php");
+require (MODEL_PATH . "Db.class.singleton.php");
+require(SITE_ROOT . "modules/products/model/DAO/product_dao.class.singleton.php");
 
 class product_bll {
 
